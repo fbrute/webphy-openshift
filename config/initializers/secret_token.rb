caa3a73d2d227e283ce19768664a8c28f843dab8
France-Nor Brute <fbrute@gmail.com>
2014-02-04 11:18:17 -0400

    Initial webphy refinerycms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webphy::Application.config.secret_token = 'f78c8306c6770456b0bcd0e0d2cd2cc4ee35f6f4c164a32c048ee56a5e209a5b8d18dfcb22b0b8412e2652d04affbe793b73d59271c8541f2c1ea82a2f3574a6'
